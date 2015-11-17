0e10b792250f6f696c72ab7f61bfddfb6e050851
sco-bo <scottebobbitt@gmail.com>
2015-11-17 12:32:40 -0500

    very rough draft, piece dropping works properly

--- connect_four.rb
+++ connect_four.rb
@@ -4,3 +4,111 @@ class Player
     @color = color
   end
 end
+
+class Board
+  attr_accessor :spaces
+  def initialize
+    @spaces = Array.new(42)
+  end
+
+  def to_s
+    output = ""
+    0.upto(41) do |position|
+      output << "#{@spaces[position] || position}".rjust(2, '0')
+      case position % 7
+      when 0, 1, 2, 3, 4, 5 then output << " | "
+      when 6 then output << "\n---------------------------------\n" unless position == 41
+      end
+    end
+    output
+  end
+
+  def space_free?(position)
+    @spaces[position].nil?
+  end
+
+  def place_piece(cell, color)
+    top_position = 
+      @spaces.each_index.select do |i|
+        i % 7 == cell && @spaces[i].nil?
+      end.max
+    @spaces[top_position] = color
+  end
+
+  def winner
+
+  end
+
+  def tie?
+    return !@spaces.include?(nil) && !winner
+  end
+end
+
+class Game
+  def initialize
+    @board = Board.new
+    @player1 = Player.new("Black")
+    @player2 = Player.new("Red")
+    @current_turn = 1
+  end
+
+  def play_game
+    puts @board
+    while !@board.winner && !@board.tie?
+      move(current_player)
+      puts @board
+    end
+    print_game_result
+  end
+
+  # private
+
+  def move(player)
+    puts "In which column would you like to drop your piece 'player #{player.color}'?"
+    choice = gets.chomp.to_i
+    if @board.space_free?(choice)
+      @board.place_piece(choice, player.color)
+      @current_turn += 1
+      puts "Player #{player.color}'s move:"
+    else
+      puts "Column unavailable! Please select another column"
+    end
+  end
+
+  def print_game_result
+    if @board.winner
+      @current_turn += 1
+      puts "Player #{current_player.color} wins!"
+      puts "Game over"
+    else
+      puts "It's a draw"
+    end
+  end
+
+  def current_player 
+    @current_turn.even? ? @player2 : @player1
+  end
+end
+
+def play_again?
+  puts "Play again? (yes or no)"
+  answer = gets.chomp.downcase
+  return answer == "yes"
+end
+
+loop do 
+  Game.new.play_game
+  unless play_again?
+    puts "Goodbye"
+    break
+  end
+end
+
+
+
+
+
+
+
+
+
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -21,3 +21,95 @@ describe Player do
     end
   end
 end
+
+describe Board do 
+  before :each do 
+    @board = Board.new
+  end  
+
+  describe "#new" do 
+    it "returns a new player object" do 
+      expect(@board).to be_an_instance_of(Board)
+    end
+  end
+
+  describe "#spaces" do 
+    it "returns an Array" do 
+      expect(@board.spaces.size).to eql 42
+    end
+  end
+
+  describe "#space_free?" do 
+    it "returns false when space is taken" do 
+      board = Board.new
+      board.spaces[0] = "red"
+      expect(board.space_free?(0)).to be false
+    end
+
+    it "returns true when space is not taken" do 
+      expect(@board.space_free?(0)).to be true
+    end
+  end
+
+  describe "#place_piece" do 
+    it "places color on space" do 
+      expect{@board.place_piece(0, "red")}.to change{@board.spaces[0]}.from(nil).to("red")
+    end
+  end
+
+  describe "#winner" do 
+    it "checks to see if the board has a winner" do 
+
+    end
+  end
+
+  describe "#tie?" do
+    it "returns true if no empty spaces and no winner" do 
+
+    end
+  end
+end
+
+describe Game do
+  before :each do 
+    @game = Game.new
+    @current_turn = 1
+  end
+
+  describe "#new" do 
+    it "returns a new game object" do 
+      expect(@game).to be_an_instance_of(Game)
+    end
+  end
+
+  describe "#play_game" do 
+
+  end
+
+  describe "#move" do 
+    
+
+  end
+
+  describe "#print_game_result" do 
+
+  end
+
+  describe "#current_player" do 
+    it "returns player1" do 
+      expect(@game.current_player).equal?(@player1)
+    end
+
+    it "returns player2" do 
+      @current_turn += 1
+      expect(@game.current_player).equal?(@player2)
+    end
+  end
+end
+
+
+
+
+
+
+
